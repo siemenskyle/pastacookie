07fe917b4a18efa1f62484bd541e9310e038e6b4
Royce Meyer (brm979) <royce.meyer@usask.ca>
2016-10-22 13:03:32 -0600

    Added resources
    There is now energy and scrap. if you move through them, it adds to your
    energy.

---
+++ Assets/Scripts/Resources/Resource.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resource : MonoBehaviour {
+
+	private int resource = 10;
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		Debug.Log (collider.gameObject.tag);
+		if (collider.gameObject.tag == "Player") {
+			if (gameObject.tag == "energy")
+				collider.gameObject.GetComponent<PlayerManagement> ().alterAmmo (resource);
+			if (gameObject.tag == "scrap")
+				collider.gameObject.GetComponent<PlayerManagement> ().alterScrap (resource);
+			this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			this.gameObject.GetComponent<Collider2D>().enabled = false;
+		}
+	}
+}

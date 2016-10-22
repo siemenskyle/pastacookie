3b5c23cbba4bf26ceb267ad995ffea5e2c4d74d6
ConnorNG <netco_nessa@hotmail.com>
2016-10-22 12:01:31 -0600

    Merge remote-tracking branch 'refs/remotes/origin/master' into combat
    # Conflicts:
    #	Assets/Scenes/Scene 1.unity
    #	Assets/Scripts/Entity/Entity.cs
    #	Assets/Scripts/Player/PlayerManagement.cs.meta
    #	Assets/Scripts/Resources/PlayerResources.cs
    #	Assets/Scripts/Resources/ResourceManager.cs.meta
    #	Assets/Scripts/Weapons/PlayerTurret.cs.meta

---
+++ Assets/Scripts/Player/PlayerManagement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManagement : Entity 
+{
+	// Use this for initialization
+	public int scrap;
+	public int energy;
+	public int ammo;
+
+	void Start ()
+	{
+		health = StartingStats.getStartingHealth ();
+		alterAmmo(StartingStats.getStartingAmmo());
+		alterEnergy(StartingStats.getStartingEnergy());
+		alterScrap(StartingStats.getStartingScrap());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+
+	public int alterAmmo(int change){
+		if (ammo + change > 0) {
+			ammo += change;
+		}
+		return ammo;
+	}
+
+	public int alterEnergy(int change){
+		if (energy + change > 0) {
+			energy += change;
+		}
+		return energy;
+	}
+
+	public int alterScrap(int change){
+		if (scrap + change > 0) {
+			scrap += change;
+		}
+		return scrap;
+	}
+
+	public int getAmmo(){
+		return ammo;
+	}
+
+	public int getEnergy(){
+		return energy;
+	}
+
+	public int getScrap(){
+		return scrap;
+	}
+}

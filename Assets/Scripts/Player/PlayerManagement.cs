7cb25a088a306edc6961654e0e4e7da45b4a69d8
Royce Meyer (brm979) <royce.meyer@usask.ca>
2016-10-21 22:07:35 -0600

    added a player script.
    need PlayerResources to make it work.

---
+++ Assets/Scripts/Player/PlayerManagement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManagement : MonoBehaviour {
+
+	//PlayerResources resources = new PlayerResources();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

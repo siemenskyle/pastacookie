b58ff91526dcb7b0fedd85dc76695bcd96c24402
siemenskyle <siemenskyle@gmail.com>
2016-10-21 23:40:32 -0600

    rename to playermove and add hooks for animator

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+	public bool altControl;
+
+	Rigidbody2D rbody;
+	Animator anim;
+
+    // Use this for initialization
+    void Start () {
+		rbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey(KeyCode.W))
+        {
+			anim.SetBool ("Forward", true);
+			rbody.AddForce(transform.up * speed);
+        }
+
+        // Reverse
+		if (Input.GetKey(KeyCode.S))
+        {
+			anim.SetBool ("Reverse", true);
+			rbody.AddForce(-transform.up * speed);
+        }
+
+        // Rotate Left or strafe left
+        if (Input.GetKey(KeyCode.Q))
+        {
+			if (altControl) {
+				transform.Rotate (0, 0, rotationSpeed);
+				anim.SetBool ("RotateLeft", true);
+			} else {
+				rbody.AddForce (-transform.right * speed);
+				anim.SetBool ("StrafeLeft", true);
+			}
+        }
+
+        // Rotate Right or strafe right
+        if (Input.GetKey(KeyCode.E))
+        {
+			if (altControl) {
+				transform.Rotate (0, 0, -rotationSpeed);
+				anim.SetBool ("RotateRight", true);
+			} else {
+				rbody.AddForce (transform.right * speed);
+				anim.SetBool ("StrafeRight", true);
+			}
+		}
+
+		// Strafe Left or rotate left
+		if (Input.GetKey(KeyCode.A))
+		{
+			if (altControl) {
+				rbody.AddForce (-transform.right * speed);
+				anim.SetBool ("StrafeLeft", true);
+			} else {
+				transform.Rotate (0, 0, rotationSpeed);
+				anim.SetBool ("RotateLeft", true);
+			}
+		}
+
+		// Strafe Right or rotate right
+		if (Input.GetKey(KeyCode.D))
+		{
+			if (altControl) {
+				rbody.AddForce (transform.right * speed);
+				anim.SetBool ("RotateRight", true);
+			} else {
+				transform.Rotate (0, 0, -rotationSpeed);
+				anim.SetBool ("RotateRight", true);
+			}
+		}
+    }
+}

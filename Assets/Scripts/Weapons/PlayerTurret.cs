d6bc27c66c14b267a2de6781b5803fc1d9d68bf0
Shane Williamson <williamson.shane.a@gmail.com>
2016-10-22 10:45:00 -0600

    Add bullet and shooting from space press.

---
+++ Assets/Scripts/Weapons/PlayerTurret.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTurret : MonoBehaviour {
+
+	Resources resources;
+	public GameObject bulletPrefab;
+	public float velocity;
+
+	// Use this for initialization
+	void Start () {
+		//this.resources = gameObject.GetComponent<Resources> ();
+	}
+
+	void FixedUpdate() {
+		if (Input.GetKeyDown ("space")) {
+			shoot (transform.position, 0f);
+		}
+	}
+
+	void shoot(Vector3 start, float angle) {
+		// Set inital position
+		start += transform.up.normalized;
+		Vector2 startingVelocity = transform.GetComponent<Rigidbody2D> ().velocity;
+		Vector3 addedVelocity = transform.up.normalized * velocity;
+		startingVelocity = new Vector2 (startingVelocity.x + addedVelocity.x, startingVelocity.y + addedVelocity.y);
+		GameObject spawnedBullet = (GameObject)Instantiate(bulletPrefab, start, transform.rotation);
+		spawnedBullet.GetComponent<Rigidbody2D> ().velocity = startingVelocity;
+
+	}
+}

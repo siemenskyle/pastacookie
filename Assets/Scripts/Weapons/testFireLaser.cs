2c6611bc9c8a6aeda4ec2b91bcd3264f45deabcb
ConnorNG <netco_nessa@hotmail.com>
2016-10-22 21:12:10 -0600

    ALL the lasers now function, they are red and pretty and stop on thier target

---
+++ Assets/Scripts/Weapons/testFireLaser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testFireLaser : MonoBehaviour {
+
+	public LayerMask mask;
+	public int stuff;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(1)) {
+			GetComponent<LaserScript> ().FireLaser (this.gameObject, Input.mousePosition,  mask);
+			stuff++;
+		}
+	}
+}

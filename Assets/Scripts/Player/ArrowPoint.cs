6d89261c56d1d1234beb80b801861a60a3a16430
ConnorNG <netco_nessa@hotmail.com>
2016-10-23 16:15:08 -0600

    made arrows destroy themselves if the thing they point to is destroyed

--- Assets/Scripts/Player/ArrowPoint.cs
+++ Assets/Scripts/Player/ArrowPoint.cs
@@ -14,14 +14,17 @@ public class ArrowPoint : MonoBehaviour {
 	void Update () {
 
 
+		if (target != null) {
+			this.transform.LookAt (target.transform.position, Vector3.up);
+			this.transform.position = playerShip.transform.position;
+			this.transform.position = this.transform.position + this.transform.forward * 3;
+			this.transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
 
-		this.transform.LookAt (target.transform.position, Vector3.up);
-		this.transform.position = playerShip.transform.position;
-		this.transform.position = this.transform.position + this.transform.forward * 3;
-		this.transform.rotation = new Quaternion (0f, 0f, 0f, 0f);
-
-		Vector3 moveDirection = gameObject.transform.position - target.transform.position;
-		float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg + 90;
-		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			Vector3 moveDirection = gameObject.transform.position - target.transform.position;
+			float angle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg + 90;
+			transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		} else {
+			Destroy (this.gameObject);
+		}
 	}
 }

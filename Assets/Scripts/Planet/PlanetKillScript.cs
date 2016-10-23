3c242b8e9d3a5d1674dfa7355d7df4b264a5145f
Shane Williamson <williamson.shane.a@gmail.com>
2016-10-23 16:21:03 -0600

    Planets kill you now.

---
+++ Assets/Scripts/Planet/PlanetKillScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetKillScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.GetComponent<PlayerManagement> ().repairDamageHull (-10000);
+		}
+	}
+}

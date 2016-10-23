0c74166f34588438b2c77717fc1cb08fdc44e412
siemenskyle <siemenskyle@gmail.com>
2016-10-22 23:12:05 -0600

    updated enemy prefab, added some initial enemy ai movement

--- Assets/Scripts/Enemy/Enemymove.cs
+++ Assets/Scripts/Enemy/Enemymove.cs
@@ -6,35 +6,54 @@ public class Enemymove : MonoBehaviour {
 	public float speed;
 	public float rotationSpeed;
 	public Transform playerLocation;
+	public float sightRange;
+	public float effectiveRange;
+	public float closeRange;
+	public bool strafeLeft;
+	public bool strafeRight;
 
 	Rigidbody2D rbody;
-	Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		rbody = GetComponent<Rigidbody2D>();
-		anim = GetComponent<Animator> ();
 	}
 
 	// Update is bad dont use it
+	/*
+	 * Will seek towards player when in sight range
+	 * will seek away from player when it is at close range
+	 * will optionally circle strafe in selected direction if at optimal range
+	 **/
 	void FixedUpdate () {
-		Vector3 rotateDir = transform.position - playerLocation.position;
-
+		float distance = Vector3.Distance (transform.position, playerLocation.position);
 		float ahead = Vector3.Dot (transform.up, (playerLocation.position - transform.position).normalized);
 		float direction = Vector3.Dot (transform.right, (playerLocation.position - transform.position).normalized);
 
+		if (distance < sightRange) {
+			if (direction > 0.05) {
+				transform.Rotate (0, 0, -rotationSpeed);
+			} else if (direction < 0.05) {
+				transform.Rotate (0, 0, rotationSpeed);
+			}
 
-		if (direction > 0.05) {
-			transform.Rotate (0, 0, -rotationSpeed);
-			anim.SetBool ("RotateRight", true);
-		} else if (direction < 0.05){
-			transform.Rotate (0, 0, rotationSpeed);
-			anim.SetBool ("RotateLeft", true);
-		}
-
-		if (ahead > 0.25) {
-			rbody.AddForce (transform.up * speed);
-			anim.SetBool ("Forward", true);	
+			float speedForce = speed;
+			Vector3 forceDirection = transform.up;
+			if (distance < closeRange)
+				speedForce *= -1;
+			else if (distance < effectiveRange){
+				if (strafeLeft)
+					forceDirection = -transform.right;
+				else if (strafeRight)
+					forceDirection = transform.right;
+				else
+					speedForce *= 0;
+			}
+			
+			
+			if (ahead > 0.25) 
+				rbody.AddForce (forceDirection * speedForce);
+			
 		}
 	}
 }
